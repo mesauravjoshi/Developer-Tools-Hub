3237b5b71d8fe58f90265008ede137221e27a957
saurav joshi <mesauravjoshii@gmail.com>
2026-03-24 20:43:02 +0530

    Stop tracking ignored folder

--- front/src/components/Store/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import sliderReducer from './Slice/SliderSlice'
-
-export const store = configureStore({
-    reducer: {
-        slider: sliderReducer,
-    }
-})
-
